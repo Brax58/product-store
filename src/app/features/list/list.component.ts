0ccc245056f48f5587e9b311567111f20bd1d36c
Brax58 <gustavoparreiras445@gmail.com>
2024-11-07 18:57:35 -0300

    Chore: Adicionando estilizacao ao componente de listar os produtos!

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { ProductsService } from '../../shared/services/products.service';
 import { Product } from '../../shared/interfaces/product.interface';
+import { MatCardModule } from '@angular/material/card';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule,MatButton],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })

e949fd8dadfe7aaaf127ab7b9a75f4c45896f162
Brax58 <gustavoparreiras445@gmail.com>
2024-11-08 09:59:38 -0300

    Chore: Incluindo o componente form no router do Angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { CreateProductComponent } from './features/forms/create-product/create-product.component';
 
-export const routes: Routes = [{
-  path: '',
-  component: ListComponent
-}];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ListComponent
+  },
+  {
+    path: 'create-product',
+    component: CreateProductComponent
+  }
+];

61dd22b505d722cfc65390458eac9c41d2c5c836
Brax58 <gustavoparreiras445@gmail.com>
2024-11-08 13:57:53 -0300

    feat: Criando Insercao de novo produto. Usando Omit para omitir propriedades.

--- src/app/features/forms/create-product/create-product.component.ts
+++ src/app/features/forms/create-product/create-product.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { ProductPayload } from './../../../shared/interfaces/payload-product.interface.ts.service';
+import { Component, inject, input } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButton } from '@angular/material/button';
+import { ProductsService } from '../../../shared/services/products.service'
 
 @Component({
   selector: 'app-create-product',
@@ -13,6 +15,8 @@ import { MatButton } from '@angular/material/button';
 })
 
 export class CreateProductComponent {
+  productService = inject(ProductsService);
+
   form = new FormGroup({
     title: new FormControl<string>('',
       {
@@ -22,6 +26,10 @@ export class CreateProductComponent {
   });
 
   onSubimit(){
-    this.form.controls.title.value;
+    this.productService.post({
+      title: this.form.controls.title.value
+    }).subscribe(() => {
+      alert('Sucess!');
+    });
   }
 }

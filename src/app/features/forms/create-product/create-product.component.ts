598c536105e71ed68ba88972c4533d2c69a1d8bd
Brax58 <gustavoparreiras445@gmail.com>
2024-11-08 11:37:57 -0300

    Refact: Ajuste estilizacao do componente create-product

--- src/app/features/forms/create-product/create-product.component.ts
+++ src/app/features/forms/create-product/create-product.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule,MatButton],
   templateUrl: './create-product.component.html',
   styleUrl: './create-product.component.scss'
 })
